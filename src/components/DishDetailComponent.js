86d392ecf8b2382d6077b7f1eb16aa70eb8ee80b
sysani <russo.anitaa@gmail.com>
2020-05-22 12:32:14 -0400

    fixed formatting on component display

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -21,25 +21,28 @@ class DishDetail extends Component {
             })
 
             return(
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardImg src={this.props.selectedDish.image} alt={this.props.selectedDish.name} />
-                            <CardBody>
-                                <CardTitle>{this.props.selectedDish.name}</CardTitle>
-                                <CardText>{this.props.selectedDish.description}</CardText>
-                            </CardBody>
-                        </Card>
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <Card>
-                            <CardBody>
-                                <CardTitle>{this.props.selectedDish.name} Comments: </CardTitle>
-                                {commentList}
-                            </CardBody>
-                        </Card>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardImg src={this.props.selectedDish.image} alt={this.props.selectedDish.name} />
+                                <CardBody>
+                                    <CardTitle>{this.props.selectedDish.name}</CardTitle>
+                                    <CardText>{this.props.selectedDish.description}</CardText>
+                                </CardBody>
+                            </Card>
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            <Card>
+                                <CardBody>
+                                    <CardTitle>{this.props.selectedDish.name} Comments: </CardTitle>
+                                    {commentList}
+                                </CardBody>
+                            </Card>
+                        </div>
                     </div>
                 </div>
+                
             );
         }
         else {
