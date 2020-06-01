7abf1f2996e21077e49db8ad136e42386332445d
sysani <russo.anitaa@gmail.com>
2020-06-01 15:47:11 -0400

    Adding contact page

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -29,6 +30,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
+                    <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />   
