a85970513659c236323fe47a47c1b0f68b67b4bf
sysani <russo.anitaa@gmail.com>
2020-06-02 13:33:37 -0400

    updating to single page app

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const DishDetail = (props) => {
 
         if (props.selectedDish != null) {
-            const commentList = props.selectedDish.comments.map((comment) => {
+            const commentList = props.comments.map((comment) => {
                 return (
                         <CardText>
                             {comment['comment']}<br /> -- {comment['author']},{" "}
@@ -15,6 +16,20 @@ const DishDetail = (props) => {
 
             return(
                 <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem>
+                                <Link to="/menu">Menu</Link>
+                            </BreadcrumbItem>
+                            <BreadcrumbItem active>
+                                {props.selectedDish.name}
+                            </BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.selectedDish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
                             <Card>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -32,6 +32,14 @@ class Main extends Component {
                 />
             )
         }
+
+        const DishWithId = ({match}) => {
+            return (
+                <DishDetail selectedDish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                />
+            )
+        }
         
         return (
             <div>
@@ -39,6 +47,7 @@ class Main extends Component {
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
+                    <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
