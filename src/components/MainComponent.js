c6188a7a469afb427b596ec519c8674e95e7009b
sysani <russo.anitaa@gmail.com>
2020-06-03 11:49:44 -0400

    Connecting about us to main component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from './HeaderComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishDetailComponent';
+import About from './AboutComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
@@ -33,6 +34,12 @@ class Main extends Component {
             )
         }
 
+        const AboutUs = () => {
+            return(
+                <About leaders={this.state.leaders} />
+            )
+        }
+
         const DishWithId = ({match}) => {
             return (
                 <DishDetail selectedDish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
@@ -49,6 +56,7 @@ class Main extends Component {
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}/>
                     <Route path="/menu/:dishId" component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' component={AboutUs} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />   
