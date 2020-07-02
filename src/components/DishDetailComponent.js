ff83a2fcb1f8afaa4202673330801e6654fdc7a2
sysani <russo.anitaa@gmail.com>
2020-07-02 14:41:36 -0400

    adding in redux thunk components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const maxLength = (len) => (value) => !(value) || (value.length <= len);
 const minLength = (len) => (value) => (value) && (value.length >= len);
@@ -123,8 +124,25 @@ function RenderDish({ dish }) {
 }
 
 const DishDetail = (props) => {
-
-    if (props.selectedDish != null) {
+    if (props.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
+                </div>
+            </div>
+        );
+    }
+    else if (props.errMess) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.errMess}</h4>
+                </div>
+            </div>
+        );
+    }
+    else if (props.selectedDish != null) {
         return(
             <div className="container">
                 <div className="row">
